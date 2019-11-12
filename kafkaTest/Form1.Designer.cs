2aaad165c3716be8fa313e306830cfb568730702
hew <hew@PC-RD-000>
2019-11-12 11:24:01 +0800

    添加项目文件。

---
+++ kafkaTest/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace kafkaTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_brokerList = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbx_topicName = new System.Windows.Forms.TextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.btn_SetProducer = new System.Windows.Forms.Button();
+            this.tbx_Msg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbx_log = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbx_brokerListConsumer = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbx_topicNameConsumer = new System.Windows.Forms.TextBox();
+            this.btn_SetConsumer = new System.Windows.Forms.Button();
+            this.rtbx_logConsumer = new System.Windows.Forms.RichTextBox();
+            this.btn_StopConsume = new System.Windows.Forms.Button();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "brokerList:";
+            // 
+            // tbx_brokerList
+            // 
+            this.tbx_brokerList.Location = new System.Drawing.Point(101, 15);
+            this.tbx_brokerList.Name = "tbx_brokerList";
+            this.tbx_brokerList.Size = new System.Drawing.Size(243, 23);
+            this.tbx_brokerList.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-442, -101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Msg:";
+            // 
+            // tbx_topicName
+            // 
+            this.tbx_topicName.Location = new System.Drawing.Point(101, 53);
+            this.tbx_topicName.Name = "tbx_topicName";
+            this.tbx_topicName.Size = new System.Drawing.Size(243, 23);
+            this.tbx_topicName.TabIndex = 1;
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(101, 150);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(75, 23);
+            this.btn_Send.TabIndex = 3;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // btn_SetProducer
+            // 
+            this.btn_SetProducer.Location = new System.Drawing.Point(101, 82);
+            this.btn_SetProducer.Name = "btn_SetProducer";
+            this.btn_SetProducer.Size = new System.Drawing.Size(90, 23);
+            this.btn_SetProducer.TabIndex = 4;
+            this.btn_SetProducer.Text = "创建生产者";
+            this.btn_SetProducer.UseVisualStyleBackColor = true;
+            this.btn_SetProducer.Click += new System.EventHandler(this.btn_SetProducer_Click);
+            // 
+            // tbx_Msg
+            // 
+            this.tbx_Msg.Location = new System.Drawing.Point(101, 121);
+            this.tbx_Msg.Name = "tbx_Msg";
+            this.tbx_Msg.Size = new System.Drawing.Size(243, 23);
+            this.tbx_Msg.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Msg:";
+            // 
+            // rtbx_log
+            // 
+            this.rtbx_log.Location = new System.Drawing.Point(25, 192);
+            this.rtbx_log.Name = "rtbx_log";
+            this.rtbx_log.Size = new System.Drawing.Size(466, 354);
+            this.rtbx_log.TabIndex = 6;
+            this.rtbx_log.Text = "";
+            this.rtbx_log.TextChanged += new System.EventHandler(this.rtbx_log_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "topicName:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(657, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "topicName:";
+            // 
+            // tbx_brokerListConsumer
+            // 
+            this.tbx_brokerListConsumer.Location = new System.Drawing.Point(733, 18);
+            this.tbx_brokerListConsumer.Name = "tbx_brokerListConsumer";
+            this.tbx_brokerListConsumer.Size = new System.Drawing.Size(243, 23);
+            this.tbx_brokerListConsumer.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(657, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "brokerList:";
+            // 
+            // tbx_topicNameConsumer
+            // 
+            this.tbx_topicNameConsumer.Location = new System.Drawing.Point(733, 56);
+            this.tbx_topicNameConsumer.Name = "tbx_topicNameConsumer";
+            this.tbx_topicNameConsumer.Size = new System.Drawing.Size(243, 23);
+            this.tbx_topicNameConsumer.TabIndex = 1;
+            // 
+            // btn_SetConsumer
+            // 
+            this.btn_SetConsumer.Location = new System.Drawing.Point(733, 85);
+            this.btn_SetConsumer.Name = "btn_SetConsumer";
+            this.btn_SetConsumer.Size = new System.Drawing.Size(90, 23);
+            this.btn_SetConsumer.TabIndex = 4;
+            this.btn_SetConsumer.Text = "创建消费者";
+            this.btn_SetConsumer.UseVisualStyleBackColor = true;
+            this.btn_SetConsumer.Click += new System.EventHandler(this.btn_SetConsumer_Click);
+            // 
+            // rtbx_logConsumer
+            // 
+            this.rtbx_logConsumer.Location = new System.Drawing.Point(549, 192);
+            this.rtbx_logConsumer.Name = "rtbx_logConsumer";
+            this.rtbx_logConsumer.Size = new System.Drawing.Size(497, 354);
+            this.rtbx_logConsumer.TabIndex = 8;
+            this.rtbx_logConsumer.Text = "";
+            this.rtbx_logConsumer.TextChanged += new System.EventHandler(this.rtbx_logConsumer_TextChanged);
+            // 
+            // btn_StopConsume
+            // 
+            this.btn_StopConsume.Location = new System.Drawing.Point(849, 85);
+            this.btn_StopConsume.Name = "btn_StopConsume";
+            this.btn_StopConsume.Size = new System.Drawing.Size(75, 23);
+            this.btn_StopConsume.TabIndex = 9;
+            this.btn_StopConsume.Text = "停止消费";
+            this.btn_StopConsume.UseVisualStyleBackColor = true;
+            this.btn_StopConsume.Click += new System.EventHandler(this.btn_StopConsume_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1058, 572);
+            this.Controls.Add(this.btn_StopConsume);
+            this.Controls.Add(this.rtbx_logConsumer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtbx_log);
+            this.Controls.Add(this.tbx_Msg);
+            this.Controls.Add(this.btn_SetProducer);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx_brokerList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbx_topicName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbx_brokerListConsumer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbx_topicNameConsumer);
+            this.Controls.Add(this.btn_SetConsumer);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_brokerList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbx_topicName;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Button btn_SetProducer;
+        private System.Windows.Forms.TextBox tbx_Msg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbx_log;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbx_brokerListConsumer;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbx_topicNameConsumer;
+        private System.Windows.Forms.Button btn_SetConsumer;
+        private System.Windows.Forms.RichTextBox rtbx_logConsumer;
+        private System.Windows.Forms.Button btn_StopConsume;
+    }
+}
+
---
+++ kafkaTest/Form1.cs
@@ -0,0 +1,219 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kafkaTest
+{
+    public partial class Form1 : Form
+    {
+        IProducer<string, string> producer;
+        IConsumer<Ignore, string> consumer;
+        CancellationTokenSource cts;
+        bool stopConsume = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbx_brokerList.Text = "172.16.19.197:9092,172.16.19.197:9093";
+            tbx_topicName.Text = "test";
+            tbx_Msg.Text = "测试消息";
+
+            tbx_brokerListConsumer.Text = "172.16.19.197:9092,172.16.19.197:9093";
+            tbx_topicNameConsumer.Text = "test";
+        }
+        private void btn_SetProducer_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbx_brokerList.Text.Trim()))
+            {
+                MessageBox.Show("请填好brokerList");
+                return;
+            }
+            if (string.IsNullOrEmpty(tbx_topicName.Text.Trim()))
+            {
+                MessageBox.Show("请填好topicName");
+                return;
+            }
+
+            var config = new ProducerConfig { BootstrapServers = tbx_brokerList.Text.Trim() };
+            producer = new ProducerBuilder<string, string>(config).Build();
+            rtbx_log.AppendText("\n-----------------------------------------------------------------------\n");
+            rtbx_log.AppendText($"Producer {producer.Name} producing on topic {tbx_topicName.Text.Trim()}.\n");
+            rtbx_log.AppendText("-----------------------------------------------------------------------\n");
+        }
+
+        private async void btn_Send_Click(object sender, EventArgs e)
+        {
+            string val = tbx_Msg.Text.Trim();
+
+            try
+            {
+                // Note: Awaiting the asynchronous produce request below prevents flow of execution
+                // from proceeding until the acknowledgement from the broker is received (at the 
+                // expense of low throughput).
+                var deliveryReport = await producer.ProduceAsync(
+                    tbx_topicName.Text.Trim(), new Message<string, string> { Key = null, Value = val });
+
+                rtbx_log.AppendText($"delivered to: {deliveryReport.TopicPartitionOffset}\n");
+                tbx_Msg.Text = "";
+            }
+            catch (ProduceException<string, string> ex)
+            {
+                rtbx_log.AppendText($"failed to deliver message: {ex.Message} [{ex.Error.Code}]\n");
+            }
+        }
+
+        private void rtbx_log_TextChanged(object sender, EventArgs e)
+        {
+            LimitLIne(rtbx_log, 100);
+            rtbx_log.SelectionStart = rtbx_log.Text.Length;
+            rtbx_log.SelectionLength = 0;
+            rtbx_log.Focus();
+        }
+
+
+        private void btn_SetConsumer_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbx_brokerListConsumer.Text.Trim()))
+            {
+                MessageBox.Show("请填好消费者brokerList");
+                return;
+            }
+            if (string.IsNullOrEmpty(tbx_topicNameConsumer.Text.Trim()))
+            {
+                MessageBox.Show("请填好消费者topicName");
+                return;
+            }
+
+            string brokerList = tbx_brokerListConsumer.Text.Trim();
+            string topics = tbx_topicNameConsumer.Text.Trim();
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = brokerList,
+                GroupId = "test-consumer",
+                EnableAutoCommit = true,
+                StatisticsIntervalMs = 5000,
+                SessionTimeoutMs = 6000,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnablePartitionEof = true
+            };
+
+            consumer = new ConsumerBuilder<Ignore, string>(config)
+                // Note: All handlers are called on the main .Consume thread.
+                .SetErrorHandler((_, e) => rtbx_logConsumer.AppendText($"Error: {e.Reason}\n"))
+                //.SetStatisticsHandler((_, json) => rtbx_logConsumer.AppendText($"Statistics: {json}\n"))
+                .SetPartitionsAssignedHandler((c, partitions) =>
+                {
+                    rtbx_logConsumer.AppendText($"Assigned partitions: [{string.Join(", ", partitions)}]\n");
+                    // possibly manually specify start offsets or override the partition assignment provided by
+                    // the consumer group by returning a list of topic/partition/offsets to assign to, e.g.:
+                    // 
+                    // return partitions.Select(tp => new TopicPartitionOffset(tp, externalOffsets[tp]));
+                })
+                .SetPartitionsRevokedHandler((c, partitions) =>
+                {
+                    rtbx_logConsumer.AppendText($"Revoking assignment: [{string.Join(", ", partitions)}]\n");
+                })
+                .Build();
+
+            consumer.Subscribe(topics);
+
+            btn_SetConsumer.Enabled = false;
+
+            Task.Run(()=> {
+                cts = new CancellationTokenSource();
+                try
+                {
+                    while (!stopConsume)
+                    {
+                        try
+                        {
+                            var consumeResult = consumer.Consume(cts.Token);
+
+                            if (consumeResult.IsPartitionEOF)
+                            {
+                                rtbx_logConsumer.AppendText(
+                                    $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.\n");
+
+                                continue;
+                            }
+
+                            rtbx_logConsumer.AppendText($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Value}\n");
+
+                            //if (consumeResult.Offset % commitPeriod == 0)
+                            //{
+                            //    // The Commit method sends a "commit offsets" request to the Kafka
+                            //    // cluster and synchronously waits for the response. This is very
+                            //    // slow compared to the rate at which the consumer is capable of
+                            //    // consuming messages. A high performance application will typically
+                            //    // commit offsets relatively infrequently and be designed handle
+                            //    // duplicate messages in the event of failure.
+                            //    try
+                            //    {
+                            //        consumer.Commit(consumeResult);
+                            //    }
+                            //    catch (KafkaException e)
+                            //    {
+                            //        Console.WriteLine($"Commit error: {e.Error.Reason}");
+                            //    }
+                            //}
+                        }
+                        catch (ConsumeException ex)
+                        {
+                            rtbx_logConsumer.AppendText($"Consume error: {ex.Error.Reason}\n");
+                            btn_SetConsumer.Enabled = true;
+                        }
+                    }
+                    rtbx_logConsumer.AppendText("Stop Consume.\n");
+                    btn_SetConsumer.Enabled = true;
+                }
+                catch (OperationCanceledException)
+                {
+                    rtbx_logConsumer.AppendText("Closing consumer.\n");
+                    consumer.Close();
+                    btn_SetConsumer.Enabled = true;
+                }
+                stopConsume = false;
+            });
+
+            
+        }
+
+        private void rtbx_logConsumer_TextChanged(object sender, EventArgs e)
+        {
+            LimitLIne(rtbx_logConsumer, 100);
+            rtbx_logConsumer.SelectionStart = rtbx_log.Text.Length;
+            rtbx_logConsumer.SelectionLength = 0;
+            rtbx_logConsumer.Focus();
+        }
+
+        private void LimitLIne(RichTextBox rtbx, int maxLength)
+        {
+            if (rtbx.Lines.Length > maxLength)
+            {
+                int moreLines = rtbx.Lines.Length - maxLength;
+                string[] lines = rtbx.Lines;
+                Array.Copy(lines, moreLines, lines, 0, maxLength);
+                Array.Resize(ref lines, maxLength);
+                rtbx.Lines = lines;
+            }
+        }
+
+        private void btn_StopConsume_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+            stopConsume = true;
+        }
+    }
+}
